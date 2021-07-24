3a63dd78a2eccac248a98c34bce6a58819f7aec4
faizmamutov <faiz.mamutov@ergonized.com>
2021-07-24 10:41:59 +0600

    Add auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {CreateUserDto} from "../users/dto/create-user.dto";
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto)
+
+    }
+
+    @Post('/registration')
+    registration(@Body() userDto: CreateUserDto) {
+        return this.authService.registration(userDto)
+
+    }
+}
+
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import {UsersModule} from "../users/users.module";
+import {JwtModule, JwtService} from "@nestjs/jwt";
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports:[
+      UsersModule,
+      JwtModule.register({
+        secret : process.env.PRIVATE_SECRET_KEY || 'SECRET',
+        signOptions:{
+          expiresIn:'24h'
+        }
+      })
+  ]
 })
 export class AuthModule {}
